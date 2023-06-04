fda953045325507eaa6efbc75286d2ee945dfaca
LuisCorrales <luis.corrales@tecsup.edu.pe>
2023-06-03 20:51:24 -0500

    Act control

--- src/main/java/com/example/funkeateapi/repository/ProductoRepository.java
+++ src/main/java/com/example/funkeateapi/repository/ProductoRepository.java
@@ -1,8 +1,12 @@
 package com.example.funkeateapi.repository;
 
+import com.example.funkeateapi.model.Categoria;
 import com.example.funkeateapi.model.Producto;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ProductoRepository extends CrudRepository<Producto, Integer>{
-	
+import java.util.List;
+
+public interface ProductoRepository extends CrudRepository<Producto, Integer> {
+    List<Producto> findByCategoria(Categoria cat);
 }
+
