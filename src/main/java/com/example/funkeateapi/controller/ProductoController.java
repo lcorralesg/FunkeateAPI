80dfe34c674e06145cda553b4cb76b1c5b5eb2a2
LuisCorrales <luis.corrales@tecsup.edu.pe>
2023-06-03 20:50:13 -0500

    Act control

--- src/main/java/com/example/funkeateapi/controller/ProductoController.java
+++ src/main/java/com/example/funkeateapi/controller/ProductoController.java
@@ -4,11 +4,12 @@ import com.example.funkeateapi.model.Categoria;
 import com.example.funkeateapi.model.Producto;
 import com.example.funkeateapi.repository.CategoriaRepository;
 import com.example.funkeateapi.repository.ProductoRepository;
+import com.nimbusds.jose.shaded.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -17,49 +18,70 @@ import java.util.Optional;
 public class ProductoController {
 	@Autowired
 	private ProductoRepository productoRepository;
-	
+
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
-	@PostMapping(path="/add") // Map ONLY POST Requests
-	  public @ResponseBody String addNewCategory (@RequestParam String name,
-			  @RequestParam String descripcion, @RequestParam String image,
-			  @RequestParam double precio, @RequestParam int categoria_id) {
-	    // @ResponseBody means the returned String is the response, not a view name
-	    // @RequestParam means it is a parameter from the GET or POST request
-				
-	    Producto p = new Producto();
-	    
-	    p.setNombre(name);
-	    p.setDescripcion(descripcion);
-	    p.setImagen(image);
-	    p.setPrecio(precio);
-	   	p.setEstado(true);
-	    p.setCreated_at(new java.util.Date());
-	    p.setUpdate_at(new java.util.Date());
-	    
-	    Optional<Categoria> cat = categoriaRepository.findById(categoria_id);
-	    if(cat.isPresent()) {
-	    	Categoria c  = cat.get();
-	    	p.setCategoria(c);
-		    productoRepository.save(p);
-		    return "Saved";
-	    }else {
-	    	return "Error";
-	    }
-	  }
-	
-	@GetMapping(path="/all")
-	  public @ResponseBody Iterable<Producto> getAllProducto() {
-	    // This returns a JSON or XML with the products
-	    return productoRepository.findAll();
-	  }
+
+	JSONObject json = new JSONObject();
+	//http://localhost:8080/products/add?name=Iron Man&descripcion=Funko de Iron Man personalizable&image=url&precio=85&categoria_id=2
+	@PostMapping(path="/add") // Ruta para añadir productos
+	public @ResponseBody String addNewCategory (@RequestParam String name,
+												@RequestParam String descripcion, @RequestParam String image,
+												@RequestParam double precio, @RequestParam int categoria_id) {
+
+		Producto p = new Producto();
+
+		p.setNombre(name);
+		p.setDescripcion(descripcion);
+		p.setImagen(image);
+		p.setPrecio(precio);
+		p.setEstado(true);
+		p.setCreated_at(new java.util.Date());
+		p.setUpdate_at(new java.util.Date());
+
+		Optional<Categoria> cat = categoriaRepository.findById(categoria_id);
+		if(cat.isPresent()) {
+			Categoria c  = cat.get();
+			p.setCategoria(c);
+			productoRepository.save(p);
+			return "Saved";
+		}else {
+			return "Error";
+		}
+	}
+
 	//http://localhost:8080/products/all
-	@GetMapping(path="/find") // Map ONLY POST Requests
-	  public @ResponseBody Optional<Producto>  findProduct (@RequestParam int producto_id) {
-	    // @ResponseBody means the returned String is the response, not a view name
-	    // @RequestParam means it is a parameter from the GET or POST request
-				
-		return productoRepository.findById(producto_id);
-	  }
+	@GetMapping(path="/all") //Ruta para obtener todos los productos
+	public @ResponseBody JSONObject getAllProducto() {
+		// This returns a JSON or XML with the products
+		json.put("count", productoRepository.count());
+		json.put("data",productoRepository.findAll());
+		return json;
+	}
+
+	// http://localhost:8080/products/find?producto_id=1
+	@GetMapping(path="/find") // Buscar productos por ID
+	public @ResponseBody JSONObject  findProduct (@RequestParam int producto_id) {
+		// @ResponseBody means the returned String is the response, not a view name
+		// @RequestParam means it is a parameter from the GET or POST request
+
+		Optional<Producto> data = productoRepository.findById(producto_id);
+		json.put("count",data.stream().count());
+		json.put("data",data);
+		return json;
+	}
+
+	// http://localhost:8080/products/findbycategory?categoria_name=SuperHeroes
+	@GetMapping(path="/findbycategory") // Obtener productos por categoria
+	public @ResponseBody JSONObject findProductbyCategory (@RequestParam String categoria_name) {
+		// @ResponseBody means the returned String is the response, not a view name
+		// @RequestParam means it is a parameter from the GET or POST request
+		List<Categoria> cat = categoriaRepository.findByNombre(categoria_name);
+		Categoria c  = cat.get(0);
+		List<Producto> data = productoRepository.findByCategoria(c);
+		json.put("count",data.stream().count());
+		json.put("data",data);
+		return json;
+	}
 }
+
